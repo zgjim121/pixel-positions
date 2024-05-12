382438df8889346da36b8aeebbd9d85d1ec34086
Zgjim Neziraj <=>
2024-05-13 00:07:51 +0200

    finishing the boring side of the design, and changing the names of the default tables

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,6 @@
+<div {{ $attributes([
+    'class' => 'p-4 bg-white/10 rounded-xl flex border border-transparent hover:border-blue-800
+                    group transition-colors duration-300']) }}
+>
+    {{ $slot }}
+</div>
